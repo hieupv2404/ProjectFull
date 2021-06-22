02ba05f8ef6bda680d4c51e50c66e9503e2024c2
Hieupv <hieupv>
2021-06-23 00:17:11 +0700

    upload file

--- src/main/java/com/nuce/group3/utils/Constant.java
+++ src/main/java/com/nuce/group3/utils/Constant.java
@@ -9,7 +9,7 @@ public class Constant {
     public static final String CACHE_PRODUCT_INFO_BY_ID = "findProductInfoById";
     public static final String CACHE_PRODUCT_STATUS_LIST_BY_ID = "findProductStatusListById";
     public static final String CACHE_FIND_SHELF_BY_ID = "findShelfById";
-    public static final String UPLOAD_PATH = "E:\\image\\";
+    public static final String UPLOAD_PATH = "C:\\image\\";
 
 
 
