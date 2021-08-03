b5028cf1b00446156bec8c21db493b8dbb838526
Hieupv <hieupv>
2021-08-04 01:27:41 +0700

    fix param

--- src/main/java/com/nuce/group3/controller/ProductStatusListController.java
+++ src/main/java/com/nuce/group3/controller/ProductStatusListController.java
@@ -83,7 +83,7 @@ public class ProductStatusListController {
                                                                          @RequestParam(name = "type", required = false) int type,
                                                                          @RequestParam(name = "branchId", required = false) Integer branchId,
                                                                          @RequestParam(name = "page", required = false) Integer page, @RequestParam(name = "size", required = false) Integer size) {
-        if (productStatusListCodeParam.isBlank()) {
+        if (productStatusListCodeParam == null) {
             productStatusListCodeParam = productStatusListCode;
         } else {
             productStatusListCode = productStatusListCodeParam;
--- src/main/java/com/nuce/group3/controller/VatController.java
+++ src/main/java/com/nuce/group3/controller/VatController.java
@@ -81,7 +81,7 @@ public class VatController {
                                                          @RequestParam(name = "branchId", required = false) Integer branchId,
 
                                                          @RequestParam(name = "page", required = false) Integer page, @RequestParam(name = "size", required = false) Integer size) {
-        if (vatCodeParam.isBlank()) {
+        if (vatCodeParam == null) {
             vatCodeParam = vatCode;
         } else {
             vatCode = vatCodeParam;
